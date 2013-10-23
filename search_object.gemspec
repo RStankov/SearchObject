6047618d9c57f2d02236c7ea462e4d15395a8dfd
Radoslav Stankov <rstankov@gmail.com>
2013-10-23 23:09:48 +0200

    ActiveRecord will be needed for plugins

--- search_object.gemspec
+++ search_object.gemspec
@@ -22,4 +22,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
   spec.add_development_dependency 'rspec', '~> 2.14'
   spec.add_development_dependency 'rspec-mocks', '>= 2.12.3'
+  spec.add_development_dependency 'activerecord', '>= 3.0.0'
+  spec.add_development_dependency 'sqlite3'
 end
---
+++ spec/spec_helper_active_record.rb
@@ -0,0 +1,19 @@
+require_relative 'spec_helper'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+
+ActiveRecord::Schema.define do
+  self.verbose = false
+
+  create_table :products, force: true do |t|
+    t.string :name
+    t.string :category_name
+    t.integer :price_cents
+
+    t.timestamps
+  end
+end
+
+class Product < ActiveRecord::Base
+end
