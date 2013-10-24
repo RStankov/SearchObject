5a7790818afe6eda98d1e39c21ed30e5cb034a5a
Radoslav Stankov <rstankov@gmail.com>
2013-10-24 17:43:18 +0200

    Move the code coverage to rake only

--- Rakefile
+++ Rakefile
@@ -1,5 +1,8 @@
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
+require 'coveralls'
+
+Coveralls.wear!
 
 RSpec::Core::RakeTask.new(:spec)
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
 require 'bundler/setup'
 require 'search_object'
-require 'coveralls'
-
-Coveralls.wear!
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :expect }
