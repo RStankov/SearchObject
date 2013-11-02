b16ddce7e82e15f399ad641a20ea7d266ee65ed3
Radoslav Stankov <rstankov@gmail.com>
2013-11-02 15:54:47 +0100

    Add more complicated filters

--- example/app/models/post_search.rb
+++ example/app/models/post_search.rb
@@ -9,14 +9,38 @@ class PostSearch
   option :category_name
 
   option :title do |scope, value|
-    scope.where 'title LIKE ?', "%#{value.gsub(/\s+/, '%')}%"
+    scope.where 'title LIKE ?', escape_search_term(value)
   end
 
   option :published do |scope, value|
     scope.where published: true if value.present?
   end
 
+  option :term do |scope, value|
+    scope.where 'title LIKE :term OR body LIKE :term', term: escape_search_term(value)
+  end
+
+  option :created_after do |scope, value|
+    date = parse_date value
+    scope.where('DATE(created_at) >= ?', date) if date.present?
+  end
+
+  option :created_before do |scope, value|
+    date = parse_date value
+    scope.where('DATE(created_at) <= ?', date) if date.present?
+  end
+
   def per_page
     25
+  end
+
+  private
+
+  def parse_date(date)
+    Date.strptime(date, '%Y-%m-%d') rescue nil
+  end
+
+  def escape_search_term(term)
+    "%#{term.gsub(/\s+/, '%')}%"
   end
 end
