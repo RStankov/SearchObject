c582bad16121281f9508d18cec34ee7e48f05c89
Radoslav Stankov <rstankov@gmail.com>
2013-11-08 15:30:56 +0100

    Add test example

---
+++ example/spec/models/post_search_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+describe PostSearch do
+  let(:user) { create_user }
+
+  def create_user
+    User.create! name: "User #{User.count + 1}"
+  end
+
+  def create(attributes = {})
+    Post.create! attributes.reverse_merge(
+      user:           user,
+      title:          'Title',
+      body:           'Body',
+      category_name:  'Tech',
+      published:      true,
+    )
+  end
+
+  def expect_search(options)
+    expect(PostSearch.new(options, 0).results)
+  end
+
+  it "can search by category name" do
+    post  = create category_name: 'Personal'
+    other = create category_name: 'Other'
+
+    expect_search(category_name: 'Personal').to eq [post]
+  end
+
+  it "can search by user_id" do
+    post  = create user: create_user
+    other = create user: create_user
+
+    expect_search(user_id: post.user_id).to eq [post]
+  end
+
+  it "can search by title" do
+    post  = create title: 'Title'
+    other = create title: 'Other'
+
+    expect_search(title: 'itl').to eq [post]
+  end
+
+  it "can search by published" do
+    post  = create published: true
+    other = create published: false
+
+    expect_search(published: true).to eq [post]
+  end
+
+  it "can search by term" do
+    post_with_body  = create body: 'pattern'
+    post_with_title = create title: 'pattern'
+    other           = create
+
+    expect_search(term: 'pattern').to eq [post_with_title, post_with_body]
+  end
+
+  it "can search by created after" do
+    post  = create created_at: 1.month.ago
+    other = create created_at: 3.month.ago
+
+    expect_search(created_after: 2.month.ago.strftime('%Y-%m-%d')).to eq [post]
+  end
+
+  it "can search by created before" do
+    post  = create created_at: 3.month.ago
+    other = create created_at: 1.month.ago
+
+    expect_search(created_before: 2.month.ago.strftime('%Y-%m-%d')).to eq [post]
+  end
+
+  it "can sort by views count" do
+    post_3 = create views_count: 3
+    post_2 = create views_count: 2
+    post_1 = create views_count: 1
+
+    expect_search(sort: 'views_count').to eq [post_3, post_2, post_1]
+  end
+end
