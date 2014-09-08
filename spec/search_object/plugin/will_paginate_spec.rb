ea9efa11c28891da8998d70304ff2036b6c0307b
Radoslav Stankov <rstankov@gmail.com>
2014-09-08 17:01:04 +0200

    Remove obsolete spec

--- spec/search_object/plugin/will_paginate_spec.rb
+++ spec/search_object/plugin/will_paginate_spec.rb
@@ -11,15 +11,6 @@ module SearchObject
           search = search_with_page
           expect(search.results.respond_to? :total_entries).to be_true
         end
-
-        describe "#results" do
-          it "paginates results" do
-            4.times { |i| Product.create name: "product_#{i}" }
-            search = search_with_page 3, 1
-
-            expect(search.results.map(&:name)).to eq %w(product_2)
-          end
-        end
       end
     end
   end
