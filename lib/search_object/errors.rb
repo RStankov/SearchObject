b7c11bc1a1a6827f91ba2e015785fc4126640b66
Radoslav Stankov <rstankov@gmail.com>
2014-09-09 11:06:54 +0200

    Minor code improvement

--- lib/search_object/errors.rb
+++ lib/search_object/errors.rb
@@ -1,12 +1,17 @@
 module SearchObject
   class MissingScopeError < ArgumentError
-    def initialize
-      super 'No scope provided. Scope can be defined on a class level or passed as an option.'
+    def initialize(message = 'No scope provided. Scope can be defined on a class level or passed as an option.')
+      super message
     end
   end
 
   class InvalidNumberError < ArgumentError
+    attr_reader :field, :number
+
     def initialize(field, number)
+      @field  = field
+      @number = number
+
       super "#{field} should be more than 0. Currently '#{number}' is given."
     end
   end
