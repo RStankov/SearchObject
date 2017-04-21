688dde1d6e3e5f5f36242fd03d2081ba562b1fca
Radoslav Stankov <rstankov@gmail.com>
2017-04-21 15:30:33 +0300

    Use `described_class` instead of full object name

--- spec/search_object/helper_spec.rb
+++ spec/search_object/helper_spec.rb
@@ -5,32 +5,34 @@ module SearchObject
   describe Helper do
     describe '.stringify_keys' do
       it 'converts hash keys to strings' do
-        hash = Helper.stringify_keys a: 1, b: nil, c: false
+        hash = described_class.stringify_keys a: 1, b: nil, c: false
         expect(hash).to eq 'a' => 1, 'b' => nil, 'c' => false
       end
 
       it 'converts ActionController::Parameters to hash' do
         params = ::ActionController::Parameters.new a: 1, b: nil, c: false
-        hash = Helper.stringify_keys params
+        hash = described_class.stringify_keys params
         expect(hash).to eq 'a' => 1, 'b' => nil, 'c' => false
       end
     end
 
     describe '.slice_keys' do
       it 'selects only given keys' do
-        hash = Helper.slice_keys({ a: 1, b: 2, c: 3 }, [:a, :b])
+        hash = described_class.slice_keys({ a: 1, b: 2, c: 3 }, [:a, :b])
         expect(hash).to eq a: 1, b: 2
       end
 
       it 'ignores not existing keys' do
-        hash = Helper.slice_keys({}, [:a, :b])
+        hash = described_class.slice_keys({}, [:a, :b])
         expect(hash).to eq({})
       end
     end
 
     describe 'camelize' do
       it "transforms :paging to 'Paging'" do
-        expect(Helper.camelize(:paging)).to eq 'Paging'
+        expect(described_class.camelize(:paging)).to eq 'Paging'
+      end
+    end
       end
     end
 
@@ -44,7 +46,7 @@ module SearchObject
           null: nil
         }
 
-        deep_copy = Helper.deep_copy(original)
+        deep_copy = described_class.deep_copy(original)
 
         original[:array][0] = 42
         original[:hash][:key] = 'other value'
