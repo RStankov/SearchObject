b0afa48f8e7a8b9a69b4c21e5d7dac983751f0c2
Tomas Novikovas <tomas.novikovas@cgtrader.com>
2019-09-18 10:55:57 +0300

    Cast possible symbol to string

--- lib/search_object/plugin/enum.rb
+++ lib/search_object/plugin/enum.rb
@@ -32,7 +32,7 @@ module SearchObject
         def apply_filter(object:, option:, enums:, scope:, value:)
           return if value.nil? || value == ''
 
-          unless enums.include? value
+          unless enums.include? value.to_s
             return handle_invalid_value(object: object, option: option, enums: enums, scope: scope, value: value)
           end
 
